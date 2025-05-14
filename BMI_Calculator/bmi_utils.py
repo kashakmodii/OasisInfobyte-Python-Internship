db1ad94fa6937ecc363e0796650b79afa44f3355
Kashak Modi <153293616+kashakmodii@users.noreply.github.com>
2025-05-14 13:16:03 +0530

    bmi_utils.py

---
+++ BMI_Calculator/bmi_utils.py
@@ -0,0 +1,12 @@
+def calculate_bmi(weight, height):
+    return weight / (height ** 2)
+
+def get_bmi_category(bmi):
+    if bmi < 18.5:
+        return "Underweight"
+    elif 18.5 <= bmi < 25:
+        return "Normal weight"
+    elif 25 <= bmi < 30:
+        return "Overweight"
+    else:
+        return "Obesity"
